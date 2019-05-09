bb580fa66a39c2d1e872ade9b8c779324c31bf28
dvVenuto <dv.venuto@gmail.com>
2019-05-09 13:33:16 -0400

    Update cnn

--- neural_density.py
+++ neural_density.py
@@ -25,6 +25,16 @@ class NeuralDensity:
         psc_reward = pow((exp(0.1 * pow(step + 1, -0.5) * max(0, pred_gain)) - 1), 0.5)
         return psc_reward
 
+    def neural_psc_getter(self, frame, step):
+        last_frame = process_density_images(frame)
+        density_input = process_density_input(last_frame)
+
+        prob = self.density_model.prob_evaluate(self.sess, density_input, False) + 1e-8
+        prob_dot = self.density_model.prob_evaluate(self.sess, density_input) + 1e-8
+        pred_gain = np.sum(np.log(prob_dot) - np.log(prob))
+        psc_reward = pow((exp(0.1 * pow(step + 1, -0.5) * max(0, pred_gain)) - 1), 0.5)
+        return psc_reward
+
     def saveModel(self,path):
         f = open('neuralDensityModel' + str(path) +'.pkl', 'wb')
         pickle.dump(self.density_model, f)
